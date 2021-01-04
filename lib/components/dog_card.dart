3f5d2855f3151f19d7b471e0ecf59b57ca4cb3b5
AZ <alvinzhu33@yahoo.com>
2021-01-03 20:40:25 -0500

    Step 3

--- lib/components/dog_card.dart
+++ lib/components/dog_card.dart
@@ -1,5 +1,6 @@
 import 'package:dog_quotes_4/models/dog.dart';
 import 'package:dog_quotes_4/screens/details_screen.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 class DogCard extends StatelessWidget {
@@ -33,8 +34,21 @@ class DogCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text(dog.name),
+    return Card(
+      child: Padding(
+        padding: EdgeInsets.symmetric(vertical: 10.0),
+        child: ListTile(
+          leading: CachedNetworkImage(
+            width: 100.0, 
+            fit: BoxFit.cover,
+            imageUrl: dog.imageUrls[0],
+            placeholder: (context, imageUrl) => Center(child: Container(height: 25.0, width: 25.0, child: CircularProgressIndicator())),
+            errorWidget: (context, imageUrl, error) => Icon(Icons.broken_image)
+          ),
+          title: Text(dog.name),
+          subtitle: Text(dog.quote),
+        ),
+      )
     );
   }
 }
